8e5afda14e9b1f2c29b853bd763f3a7b88cb0cbc
Neeladevi-meruva <130243105+Neeladevi-meruva@users.noreply.github.com>
2023-08-04 08:21:02 +0530

    Create Defaultvalues.java

---
+++ Defaultvalues.java
@@ -0,0 +1,22 @@
+class Defaultvalues
+  {
+    static int a;
+    static float b;
+    static char c;
+    static double d;
+    static boolean bo;
+    static byte by;
+    static short s;
+    static String l;
+    public static vooid main(String args[])
+    {
+      System.out.println(a);
+      System.out.println(b);
+      System.out.println(c);
+      System.out.println(d);
+      System.out.println(bo);
+      System.out.println(by);
+      System.out.println(s);
+      System.out.println(l);
+    }
+  }
