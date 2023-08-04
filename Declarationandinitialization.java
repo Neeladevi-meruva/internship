360100180aa2f80b3eb7afafd28faf47e847283e
Neeladevi-meruva <130243105+Neeladevi-meruva@users.noreply.github.com>
2023-08-04 09:38:12 +0530

    Create Declarationandinitialization.java

---
+++ Declarationandinitialization.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Declarationandinitialization
+  {
+    public static void main(String args[])
+    {
+      int a;//declaration
+     int b=10;//initialization
+      Scanner s=new Scanner(System.in);
+      a=s.nextInt();
+      System.out.println(a);
+      System.out.println(b);
+    }
+  }
