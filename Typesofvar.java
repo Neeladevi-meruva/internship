6faafad63125544ae0d97a47f752937cd44cd805
Neeladevi-meruva <130243105+Neeladevi-meruva@users.noreply.github.com>
2023-08-04 09:26:38 +0530

    Create Typesofvar.java

---
+++ Typesofvar.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Pro12
+{
+	public static void main(String...x)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the value : ");
+		int PascalCase=sc.nextInt();
+System.out.println("enter the value : ");
+               float camelCase=sc.nextFloat();
+System.out.println("enter the value : ");
+               String snake_case=sc.next();
+System.out.println("enter the value : ");
+               double $symchk=sc.nextDouble();
+        System.out.println(PascalCase);
+        System.out.println(camelCase);
+        System.out.println(snake_case);
+System.out.println($symchk);
+	}
+}
